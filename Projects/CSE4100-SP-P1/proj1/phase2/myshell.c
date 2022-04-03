16511eb125d944bd7dd7ad95469ce8cc78d4cd1c
Luchs41 <kokobo6@gmail.com>
2022-04-03 21:06:31 +0900

    test

--- Projects/CSE4100-SP-P1/proj1/phase2/myshell.c
+++ Projects/CSE4100-SP-P1/proj1/phase2/myshell.c
@@ -108,21 +108,79 @@ int builtin_command(char **argv)
 /* parseline - Parse the command line and build the argv array */
 int parseline(char *buf, char **argv) 
 {
-	char *delim;         /* Points to first space delimiter */
-	int argc;            /* Number of args */
-	int bg;              /* Background job? */
+	// char *delim;         /* Points to first space delimiter */
+	// int argc;            /* Number of args */
+	// int bg;              /* Background job? */
 
-	buf[strlen(buf)-1] = ' ';  /* Replace trailing '\n' with space */
+	// buf[strlen(buf)-1] = ' ';  /* Replace trailing '\n' with space */
+	// while (*buf && (*buf == ' ')) /* Ignore leading spaces */
+	// 	buf++;
+	// /* Build the argv list */
+	// argc = 0;
+	// while ((delim = strchr(buf, ' '))) {
+	// 	argv[argc++] = buf;
+	// 	*delim = '\0';
+	// 	buf = delim + 1;
+	// 	while (*buf && (*buf == ' ')) /* Ignore spaces */
+	// 		buf++;
+	// }
+	// argv[argc] = NULL;
+
+	// if (argc == 0)  /* Ignore blank line */
+	// 	return 1;
+
+	// /* Should the job run in the background? */
+	// if ((bg = (*argv[argc-1] == '&')) != 0)
+	// 	argv[--argc] = NULL;
+
+	// return bg;
+	char *delim;		/* delimiter */
+	int argc;			/* Number of args */
+	int bg;				/* Background job? */
+	char *space;		/* Points to first space delimiter */
+	char *dquotes1;		/* Points to first double quotes delimiter */
+	char *dquotes2;		/* Points to second double quotes delimiter */
+	char *squotes1;		/* Points to first single quotes delimiter */
+	char *squotes2;		/* Points to second single quotes delimiter */
+	char *backslash;	/* Points to second backslash delimiter */
+
+	buf[strlen(buf) - 1] = ' ';  /* Replace trailing '\n' with space */
 	while (*buf && (*buf == ' ')) /* Ignore leading spaces */
 		buf++;
+
 	/* Build the argv list */
 	argc = 0;
-	while ((delim = strchr(buf, ' '))) {
-		argv[argc++] = buf;
-		*delim = '\0';
-		buf = delim + 1;
-		while (*buf && (*buf == ' ')) /* Ignore spaces */
-			buf++;
+
+	while ((space = strchr(buf, ' '))) {
+
+		delim = space;
+		dquotes1 = strchr(buf, '\"');
+		squotes1 = strchr(buf, '\'');
+		if (dquotes1 != 0 && dquotes1 < delim) delim = dquotes1;
+		if (squotes1 != 0 && squotes1 < delim) delim = squotes1;
+		if (delim == dquotes1) {			/* double quotes*/
+			dquotes2 = strchr(delim + 1, '\"');
+			argv[argc++] = delim + 1;
+			*dquotes2 = '\0';
+			buf = dquotes2 + 1;
+			while (*buf && (*buf == ' '))	/* Ignore spaces */
+				buf++;
+		}
+		else if (delim == squotes1) {		/* single quotes*/
+			squotes2 = strchr(delim + 1, '\'');
+			argv[argc++] = buf + 1;
+			*squotes2 = '\0';
+			buf = squotes2 + 1;
+			while (*buf && (*buf == ' ')) /* Ignore spaces */
+				buf++;
+		}
+		else {
+			argv[argc++] = buf;
+			*delim = '\0';
+			buf = delim + 1;
+			while (*buf && (*buf == ' ')) /* Ignore spaces */
+				buf++;
+		}
 	}
 	argv[argc] = NULL;
 
@@ -130,7 +188,7 @@ int parseline(char *buf, char **argv)
 		return 1;
 
 	/* Should the job run in the background? */
-	if ((bg = (*argv[argc-1] == '&')) != 0)
+	if ((bg = (*argv[argc - 1] == '&')) != 0)
 		argv[--argc] = NULL;
 
 	return bg;
