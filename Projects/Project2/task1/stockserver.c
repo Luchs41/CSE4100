1608207e4af56b698d66585e9232ea88b53fefc8
Lee Keonyoung <kokobo6@gmail.com>
2022-05-07 05:48:35 +0000

    default

--- Projects/Project2/task1/stockserver.c
+++ Projects/Project2/task1/stockserver.c
@@ -1,7 +1,7 @@
 /* 
- * echoserveri.c - An iterative echo server 
+ * stockserver.c - An stock server 
  */ 
-/* $begin echoserverimain */
+/* $begin stockserverimain */
 #include "csapp.h"
 
 void echo(int connfd);
@@ -14,20 +14,19 @@ int main(int argc, char **argv)
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
     if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
-	exit(0);
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(0);
     }
 
     listenfd = Open_listenfd(argv[1]);
     while (1) {
-	clientlen = sizeof(struct sockaddr_storage); 
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, 
-                    client_port, MAXLINE, 0);
-        printf("Connected to (%s, %s)\n", client_hostname, client_port);
-	echo(connfd);
-	Close(connfd);
+		clientlen = sizeof(struct sockaddr_storage); 
+		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+	    Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+		printf("Connected to (%s, %s)\n", client_hostname, client_port);
+		echo(connfd);
+		Close(connfd);
     }
     exit(0);
 }
-/* $end echoserverimain */
+/* $end stockserverimain */
